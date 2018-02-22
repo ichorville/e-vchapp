092a0077d2e705a06f7e1a460f3a64c10a472bc3
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-02-22 09:36:53 +0530

    Generated Purchase-orders module

---
+++ src/app/views/purchase-order/purchase-order.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PurchaseOrderComponent } from './purchase-order.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PurchaseOrderComponent]
+})
+export class PurchaseOrderModule { }
