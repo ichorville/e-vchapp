020b8936ad2e1fef7830342e28811070cedbc3be
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-02-22 10:06:33 +0530

    Necessary module imports added

--- src/app/views/purchase-order/purchase-order.module.ts
+++ src/app/views/purchase-order/purchase-order.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+
 import { PurchaseOrderComponent } from './purchase-order.component';
+import { PurchaseOrderListComponent } from './purchase-order-list/purchase-order-list.component';
+import { PurchaseOrderConfigComponent } from './purchase-order-config/purchase-order-config.component';
+
+import { PurchaseOrderRoutes } from './purchase-order.routing';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [PurchaseOrderComponent]
+	imports: [
+		CommonModule,
+		RouterModule.forChild(PurchaseOrderRoutes),
+	],
+	declarations: [
+		PurchaseOrderComponent, 
+		PurchaseOrderListComponent, 
+		PurchaseOrderConfigComponent
+	]
 })
 export class PurchaseOrderModule { }
