98b3d707f2c598a6e0e36e4032b11a8e31119c59
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-03-02 10:48:47 +0530

    Necessary dependancies added to File-upload module

--- src/app/views/file-upload/file-upload.module.ts
+++ src/app/views/file-upload/file-upload.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { 
+	MatInputModule,
+	MatDatepickerModule, 
+	MatNativeDateModule,
+	MatCardModule,
+	MatSelectModule, 
+	MatListModule,
+	MatButtonModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { QuillModule } from 'ngx-quill';
+
 import { FileUploadComponent } from './file-upload.component';
 
+import { FileUploadRoutes } from './file-upload.routing';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [FileUploadComponent]
+	imports: [
+		CommonModule,
+		FormsModule, 
+		ReactiveFormsModule,
+		MatInputModule,
+		MatDatepickerModule, 
+		MatNativeDateModule,
+		MatCardModule,
+		MatSelectModule,
+		MatListModule,
+		MatButtonModule,
+		FlexLayoutModule,
+		QuillModule,
+		RouterModule.forChild(FileUploadRoutes)
+	],
+	declarations: [
+		FileUploadComponent
+	]
 })
 export class FileUploadModule { }
