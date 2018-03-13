191cb228e1488f3cf3eeda1e15f72696856f464e
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-03-13 16:55:28 +0530

    File upload component functionality mimmiced in Serial upload module

---
+++ src/app/views/serial/serial-upload/serial-upload.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FileUploader, FileLikeObject } from 'ng2-file-upload';
+@Component({
+	selector: 'app-serial-upload',
+	templateUrl: './serial-upload.component.html',
+	styleUrls: ['./serial-upload.component.css']
+})
+export class SerialUploadComponent implements OnInit {
+
+	public uploader: FileUploader = new FileUploader({ url: '`https://10.20.10.21/file/upload' });
+	public hasBaseDropZoneOver: boolean = false;
+	console = console;
+
+	constructor () { }
+
+	ngOnInit() {
+		this.uploader.onAfterAddingFile = (file) => { 
+			this.uploader.queue.forEach(element => {
+				if (element['file']['name'] == file['file']['name']) {
+					let arr = [];
+					this.uploader.queue.forEach(element => {
+						if (element['file']['name'] == file['file']['name']) {
+							arr.push(element);
+						}
+					});
+					if (arr.length > 1) {
+						this.uploader.removeFromQueue(element);
+					}
+				}
+				if ((element['file']['type'] == 'application/vnd.ms-excel') || (element['file']['type'] == 'text/plain')) {
+
+				} else {
+					this.uploader.removeFromQueue(element);
+				}
+			});
+		};
+	}
+
+	public fileOverBase(e: any): void {
+		this.hasBaseDropZoneOver = e;
+	}
+}
