2a730cbab039d878c806e1384abb6a8d43313f02
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-03-14 15:39:28 +0530

    New component dependencied added

--- src/app/views/serial/serial-mismatch/serial-mismatch.module.ts
+++ src/app/views/serial/serial-mismatch/serial-mismatch.module.ts
@@ -16,10 +16,13 @@ import {
 } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { QuillModule } from 'ngx-quill';
+import { SimpleDatatableModule } from '../../shared/simple-datatable/simple-datatable.module';
+import { AppLoaderModule } from '../../../services/app-loader/app-loader.module';
 
 import { SerialMismatchComponent } from './serial-mismatch.component';
-
 import { SerialMismatchRoutes } from './serial-mismatch.routing';
+import { SerialMismatchService } from './serial-mismatch.service';
+import { AppLoaderService } from '../../../services/app-loader/app-loader.service';
 
 @NgModule({
 	imports: [
@@ -38,10 +41,16 @@ import { SerialMismatchRoutes } from './serial-mismatch.routing';
 		MatTooltipModule,
 		FlexLayoutModule,
 		QuillModule,
+		SimpleDatatableModule,
+		AppLoaderModule,
 		RouterModule.forChild(SerialMismatchRoutes)
 	],
 	declarations: [
 		SerialMismatchComponent
+	],
+	providers: [
+		SerialMismatchService,
+		AppLoaderService
 	]
 })
 export class SerialMismatchModule { }
